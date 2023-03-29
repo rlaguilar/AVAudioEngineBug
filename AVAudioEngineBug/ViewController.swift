d5290fd4eccb204da02d42201ed4e278d33d87dd
Reynaldo Aguilar <rlac1990@gmail.com>
2023-03-29 11:15:20 +1100

    Reproduce bug

--- AVAudioEngineBug/ViewController.swift
+++ AVAudioEngineBug/ViewController.swift
@@ -1,19 +1,35 @@
-//
-//  ViewController.swift
-//  AVAudioEngineBug
-//
-//  Created by Reynaldo Aguilar on 29/3/2023.
-//
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    let engine = AVAudioEngine()
+    let format = AVAudioFormat(standardFormatWithSampleRate: 48000, channels: 2)!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        try! engine.enableManualRenderingMode(.offline, format: format, maximumFrameCount: 1024)
+        try! engine.start()
+
+        let player = AVAudioPlayerNode()
+        configureEngine(player: player, useVarispeed: true)
+        player.play()
+
+        print("Finished running")
     }
 
+    func configureEngine(player: AVAudioPlayerNode, useVarispeed: Bool) {
+        engine.attach(player)
 
+        guard useVarispeed else {
+            engine.connect(player, to: engine.mainMixerNode, format: format)
+            return
+        }
+
+        let varispeed = AVAudioUnitVarispeed()
+        engine.attach(varispeed)
+        engine.connect(player, to: varispeed, format: format)
+        engine.connect(varispeed, to: engine.mainMixerNode, format: format)
+    }
 }
-
